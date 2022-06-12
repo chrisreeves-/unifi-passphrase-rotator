990b4bf65e2bd0a173473b69392eabf33d6063de
Christopher Reeves <chris@reeves.net.nz>
2022-06-12 18:15:09 +1000

    Getting sites

--- rotator.py
+++ rotator.py
@@ -9,27 +9,25 @@ username = input("Please enter your username to access the Unifi controller: ")
 password = input("Please enter your password to access the Unifi controller: ")
 SSID = input("Please enter the SSID you want to rotate: ")
 
+
 # Remove unverified HTTPS request warning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-def unifi_auth():
-    headers = {"Accept": "application/json",
-               "Content-Type": "application/json"}
-    controller = f"https://{ip}:8443/api/login"
-    body = {
-        "username": f"{username}",
-        "password": f"{password}"
-    }
-    session = requests.Session()
-    response = session.post(controller, headers=headers, data=json.dumps(body), verify=False)
-    api_data = response.json()
-    print(api_data)
+headers = {"Accept": "application/json",
+           "Content-Type": "application/json"}
+controller = f"https://{ip}:8443/api/login"
+body = {
+    "username": f"{username}",
+    "password": f"{password}"
+}
+session = requests.Session()
+response = session.post(controller, headers=headers, data=json.dumps(body), verify=False)
+api_data = response.json()
+#print(api_data)
 
-def unifi_wireless_config():
-    print("hello world")
-
-def unifi_phrase_rotate():
-    print("hello world")
-
-def main():
-    print("do a thing")
+# Set Sites
+getSitesUrl = 'api/self/sites'
+url = f"https://{ip}:8443/api/self/sites"
+response = session.get(url, headers=headers, verify=False)
+api_data = response.json()
+json = json.dumps(api_data, indent=1)
