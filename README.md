1eb889ec2a0bbb101540b8eb601e4c2ae6cd19cb
Christopher Reeves <chris@reeves.net.nz>
2022-06-12 15:58:39 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # unifi-passphrase-rotator
+
+## Overview
+
+This simple script rotates Unifi controller wifi passwords
+
+## References
+
+https://www.youtube.com/watch?v=t-_wVzULmUY
---
+++ rotator.py
@@ -0,0 +1,18 @@
+import random
+
+ip = input("Please enter the Unifi controllers ip address: ")
+username = input("Please enter your username to access the Unifi controller: ")
+password = input("Please enter your password to access the Unifi controller: ")
+SSID = input("Please enter the SSID you want to rotate: ")
+
+def unifi_auth():
+    print("hello world")
+
+def unifi_wireless_config():
+    print("hello world")
+
+def unifi_phrase_rotate():
+    print("hello world")
+
+def main():
+    print("do a thing")
