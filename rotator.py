6c316548bb55d8ab64b904f84f9482b5aaf34bf5
Christopher Reeves <chris@reeves.net.nz>
2022-06-14 15:30:04 +1000

    Adding rotation of passphrase

--- rotator.py
+++ rotator.py
@@ -7,14 +7,15 @@ port = input("Please enter the Unifi controllers port numnber: ")
 username = input("Please enter your username to access the Unifi controller: ")
 password = input("Please enter your password to access the Unifi controller: ")
 SSID = input("Please enter the SSID you want to rotate: ")
+newpssword = "THISISTHENEWPASSWORD"
 
 # Remove unverified HTTPS request warning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # Auth Headers
+controller = f"https://{ip}:{port}/api/login"
 headers = {"Accept": "application/json",
            "Content-Type": "application/json"}
-controller = f"https://{ip}:{port}/api/login"
 body = {
     "username": f"{username}",
     "password": f"{password}"
@@ -23,16 +24,26 @@ body = {
 # Auth to Unifi Controller
 session = requests.Session()
 response = session.post(controller, headers=headers, data=json.dumps(body), verify=False)
-api_data = response.json()
+auth_data = response.json()
 
 # Set Sites
 url = f"https://{ip}:{port}/api/self/sites"
 response = session.get(url, headers=headers, verify=False)
-api_data = response.json()
-for name in api_data["data"]:
+sites_data = response.json()
+for name in sites_data["data"]:
     sitename = (name["name"])
 
 # Get Config
 url = f"https://{ip}:{port}/api/s/{sitename}/rest/wlanconf"
 response = session.get(url, headers=headers, verify=False)
-api_data = response.json()
+config_data = response.json()
+for id in config_data["data"]:
+    id = (id["_id"])
+
+# Change Password
+body = {
+    "x_passphrase": f"{newpassword}"
+}
+url = f"https://{ip}:{port}/api/s/{sitename}/rest/wlanconf/{id}"
+response = session.get(url, headers=headers, data=json.dumps(body), verify=False)
+result_data = response.json()
